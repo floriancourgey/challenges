112fa326721561a94b64935c783b69ed96c3ed60
Florian Courgey <floriancourgey@users.noreply.github.com>
2018-08-15 13:34:57 -0400

    * gravure mecanique with pyglet

--- programmation/La gravure mécanique/main.py
+++ programmation/La gravure mécanique/main.py
@@ -1,160 +1,78 @@
-#! /usr/bin/python
+#! /usr/bin/env python3
 # coding: utf-8
-
-"""
-Florian Courgey
-"""
-
-# imports
-import urllib2
-import re
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
 import config
+import pyglet
+import re
+import time
 
-# constantes
-url = 'https://www.newbiecontest.org/epreuves/prog/proggravure.php/'
+window = pyglet.window.Window(1200, 300)
 
-opener = urllib2.build_opener()
-opener.addheaders.append(('Cookie', config.COOKIE))
-print('Chargement de '+url)
-html = opener.open(url).read().decode("utf-8")
-print('html reçu')
-html = html.replace("O1234(GRAVURE)", "")
-html = html.replace("G28G91Z0.Y0.", "")
-html = html.replace("M6T1", "")
-html = html.replace("G0G90G54X0.Y0.M3S7500F250.", "")
-html = html.replace("G43H1Z2.M8", "")
-html = html.replace("G1Z-.1F125.", "")
-html = html.replace("G0Z2.", "")
-print('html assaini')
-regex = "(?P<cle>[GXY])(?P<valeur>[\d\.\-]+)"
-facteur = 10;
-# liste de tous les x pour avoir leur min/max
-xListe = []
-xMin = 0*facteur
-xMax = 60*facteur
-# liste de tous les y pour avoir leur min/max
-yListe = []
-yMin = -4*facteur
-yMax = 11*facteur
-# liste des clés/valeurs
-liste = []
+i=0 # number of lines drawn
+facteur = 20 # scale text smaller/bigger
+f = open('samples/1.gcode', 'r')
 
-class SimpleClass(object):
-    def __str__(self):
-     return self.cle+":"+str(self.valeur)
+relativeX = 0 # last x (modified by moveRelativeToAbsolute() )
+relativeY = 0 # last y (modified by moveRelativeToAbsolute() )
 
-for match in re.finditer(regex, html):
-    cleRaw = match.group('cle')
-    valeurRaw = match.group('valeur')
-    cle = cleRaw
-    valeur = float(valeurRaw)*facteur
-    objet = SimpleClass();
-    objet.cle = cle;
-    objet.valeur = valeur;
-    liste.append(objet)
-    if cle == "X":
-        xListe.append(valeur)
-    if cle == "Y":
-        yListe.append(valeur)
-print('liste buildée')
-print(str(min(xListe))+' < x < '+str(max(xListe)))
-print(str(min(yListe))+' < y < '+str(max(yListe)))
-print(html)
+absoluteX = 10 # current x origin (modified by G52)
+absoluteY = 30 # current y origin (modified by G52)
 
-# GUI
+def drawLineAbsolute(fromX, fromY, toX, toY):
+    global absoluteX
+    global absoluteY
+    global i
+    i+=1
+    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+        ('v2f', (fromX+absoluteX, fromY+absoluteY, toX+absoluteX, toY+absoluteY)),
+        ('c3B', (0, 255, 0, 255, 0, 255))
+    )
+    print('draw abs', (fromX+absoluteX, fromY+absoluteY, toX+absoluteX, toY+absoluteY))
+    # on trace 1 i sur 2
+    if i%2 == 1:
+        pyglet.text.Label(text=str(i), x=fromX+absoluteX, y=fromY+absoluteY).draw()
 
+def drawLineFromRelativeToAbsolute(toX, toY):
+    global relativeX
+    global relativeY
+    if toX == 0:
+        toX = relativeX
+    if toY == 0:
+        toY = relativeY
+    drawLineAbsolute(relativeX, relativeY, toX, toY)
+    moveRelativeToAbsolute(toX, toY)
 
+def moveRelativeToAbsolute(toX, toY):
+    global relativeX
+    global relativeY
+    if toX != 0:
+        relativeX = toX
+    if toY != 0:
+        relativeY = toY
 
-def on_draw(widget, cr):
-    print('on_draw')
-    cr.set_source_rgb(0.6, 0.6, 0.6)
-    cr.set_line_width(.2*facteur)
+@window.event
+def on_draw():
+    global absoluteX
+    global absoluteY
 
-    # cr.line_to(100,100)
-    xBase = 0
-    yBase = 0
-    xBaseADefinir = True
-    yBaseADefinir = True
-
-    xPrecedent = 0
-    yPrecedent = 0
-
-    gPrecedent = False
-
-    i = 0;
-
-    for objet in liste:
-        # print(objet)
-        if objet.cle == "G":
-            # cht couleur
-            cr.set_source_rgba(i*0.1, 0.6, 0.6, 0.2)
-            # tracé
-            cr.stroke()
-            # incrémentation lettre
-            i += 1
-            print ("i "+str(i))
-            # il faut redéfinir la base (x;y)
-            xBaseADefinir = True
-            yBaseADefinir = True
+    window.clear()
+    regex = "(?P<g52>G52)?(X(?P<x>[\-\d\.]+))?(Y(?P<y>[\-\d\.]+))?"
+    for match in re.finditer(regex, f.read()):
+        x = y = 0
+        if match.group('x'):
+            x = round(float(match.group('x'))*facteur, 1)
+        if match.group('y'):
+            y = round(float(match.group('y'))*facteur, 1)
+        if x==0 and y==0:
+            continue
+        # change origin
+        if match.group('g52'):
+            print('- line "'+match.group(0).strip()+'": G52 :', x, y)
+            absoluteX = x
+            # absoluteY = y # we don't want to change the y axis
         else:
-            if objet.cle == "X":
-                # si base à définir,
-                # - on set le x de la base
-                # - on move_to x
-                if xBaseADefinir:
-                    xBase = objet.valeur
-                    xBaseADefinir = False
-                    cr.move_to(xBase, yPrecedent)
-                    xPrecedent = xBase
-                # sinon on line_to
-                else:
-                    cr.line_to(xBase + objet.valeur, yPrecedent)
-                    xPrecedent = xBase + objet.valeur
-            elif objet.cle == "Y":
-                # si base à définir,
-                # - on set le y de la base
-                # - on move_to y
-                if yBaseADefinir:
-                    yBase = objet.valeur
-                    yBaseADefinir = False
-                    cr.move_to(xPrecedent, yBase)
-                    yPrecedent = yBase
-                # sinon on line_to
-                else:
-                    cr.line_to(xPrecedent, yBase + objet.valeur)
-                    yPrecedent = yBase + objet.valeur
+            print('- line "'+match.group(0).strip()+'": draw :', x, y)
+            drawLineFromRelativeToAbsolute(x,y)
 
+        continue
 
-
-
-    # cr.move_to(240, 40)
-    # cr.line_to(240, 160)
-    # cr.line_to(350, 160)
-    # cr.fill()
-    #
-    # cr.move_to(380, 40)
-    # cr.line_to(380, 160)
-    # cr.line_to(450, 160)
-    # cr.curve_to(440, 155, 380, 145, 380, 40)
-
-
-def main():
-    builder = Gtk.Builder()
-    builder.add_from_file("main.glade")
-    win = builder.get_object("mainWindow")
-    win.set_size_request(1280,640)
-
-
-    zone = builder.get_object("zoneDeDessin")
-    zone.connect('draw', on_draw);
-
-    win.connect("delete-event", Gtk.main_quit)
-
-    win.show_all()
-    Gtk.main()
-
-if __name__ == "__main__":
-    main()
+pyglet.app.run()
