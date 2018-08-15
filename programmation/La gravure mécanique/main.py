5beb8f6e7ea8e444c953e6498f1dd68e7dcf6dca
Florian courgey (Fresh) <florian@floriancourgey.com>
2018-08-15 16:03:46 -0400

    * prog/gravure change to Pillow instead of Pyglet

--- programmation/La gravure mécanique/main.py
+++ programmation/La gravure mécanique/main.py
@@ -1,16 +1,14 @@
 #! /usr/bin/env python3
 # coding: utf-8
 import config
-import pyglet
 import re
 import time
+from PIL import Image, ImageDraw
 
-window = pyglet.window.Window(1200, 300)
-
-i=0 # number of lines drawn
+i=-1 # number of lines drawn
 rapidMode = True # True = don't draw, just move / False = draw
 scaleFactor = 20 # scale text smaller/bigger
-filename = 'samples/3.gcode'
+filename = 'samples/1.gcode'
 f = open(filename, 'r')
 
 relativeX = 0 # last x (modified by moveRelativeToAbsolute() )
@@ -19,20 +17,22 @@ relativeY = 0 # last y (modified by moveRelativeToAbsolute() )
 absoluteX = 10 # current x origin (modified by G52)
 absoluteY = scaleFactor*4 # current y origin (modified by G52)
 
+iPolygon = 0;
+polygons = []
+im = Image.new('RGB', (1200,300))
+draw = ImageDraw.Draw(im)
+
 def drawLineAbsolute(fromX, fromY, toX, toY):
     global absoluteX
     global absoluteY
-    global i
-    i+=1
-    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-        ('v2f', (fromX+absoluteX, fromY+absoluteY, toX+absoluteX, toY+absoluteY)),
-        ('c3B', (0, 255, 0, 0, 255, 0)) # green
-        # ('c3B', (0, 255, 0, 255, 0, 255)) # green to purple
-    )
-    print('draw abs', (fromX+absoluteX, fromY+absoluteY, toX+absoluteX, toY+absoluteY))
-    # draw a text helper at the start of the line (for debugging)
-    # if i%2 == 1:
-    #     pyglet.text.Label(text=str(i), x=fromX+absoluteX, y=fromY+absoluteY).draw()
+
+    global polygons
+    global iPolygon
+
+    print('polygons len('+str(len(polygons))+'), iPolygon '+str(iPolygon))
+    if len(polygons) <= iPolygon:
+        polygons.append([])
+    polygons[iPolygon].append( (toX+absoluteX, im.size[1]-toY-absoluteY) )
 
 def drawLineFromRelativeToAbsolute(toX, toY):
     global relativeX
@@ -52,52 +52,54 @@ def moveRelativeToAbsolute(toX, toY):
     if toY is not None:
         relativeY = toY
 
-
-@window.event
-def on_draw():
-    global absoluteX
-    global absoluteY
-
-    window.clear()
-    regex = "(?P<g0>G0)?(?P<g1>G1)?(?P<g52>G52)?(X(?P<x>[\-\d\.]+))?(Y(?P<y>[\-\d\.]+))?"
-    for iLine,line in enumerate(f):
-        line = line.strip().upper() # strip + upper
-        print('- line '+str(iLine+1)+':', line)
-        match = re.match(regex, line)
-        # Rapid mode
-        if not match:
-            continue
-        if match.group('g0'):
-            print('* G0: Rapid mode ON')
-            rapidMode = True
-            continue
-        if match.group('g1'):
-            print('* G1: Rapid mode OFF')
-            rapidMode = False
-            continue
-        # X/Y with or without G52
-        x = y = None
-        if match.group('x'):
-            x = round(float(match.group('x'))*scaleFactor, 1)
-        if match.group('y'):
-            y = round(float(match.group('y'))*scaleFactor, 1)
-        if x==None and y==None:
-            continue
-        # change origin
-        if match.group('g52'):
-            print('* G52: (X:'+str(x)+', Y:'+str(y)+')')
-            absoluteX = x
-            # absoluteY = y # never change y axis
-        else:
-            if rapidMode:
-                print('* move: (X:'+str(x)+', Y:'+str(y)+')')
-                moveRelativeToAbsolute(x,y)
-            else:
-                print('* draw: (X:'+str(x)+', Y:'+str(y)+')')
-                drawLineFromRelativeToAbsolute(x,y)
-
+regex = "(?P<g0>G0)?(?P<g1>G1)?(?P<g52>G52)?(X(?P<x>[\-\d\.]+))?(Y(?P<y>[\-\d\.]+))?"
+for iLine,line in enumerate(f):
+    line = line.strip().upper() # strip + upper
+    # print('- line '+str(iLine+1)+':', line)
+    match = re.match(regex, line)
+    # Rapid mode
+    if not match:
         continue
+    if match.group('g0'):
+        print('* G0: Rapid mode ON')
+        rapidMode = True
+        continue
+    if match.group('g1'):
+        print('* G1: Rapid mode OFF')
+        rapidMode = False
+        print('polygons len('+str(len(polygons))+'), iPolygon '+str(iPolygon))
+        if len(polygons) <= iPolygon:
+            polygons.append([])
+        iPolygon+=1
+        continue
+    # X/Y with or without G52
+    x = y = None
+    if match.group('x'):
+        x = round(float(match.group('x'))*scaleFactor, 1)
+    if match.group('y'):
+        y = round(float(match.group('y'))*scaleFactor, 1)
+    if x==None and y==None:
+        continue
+    # change origin
+    if match.group('g52'):
+        print('* G52: (X:'+str(x)+', Y:'+str(y)+')')
+        absoluteX = x
+        # absoluteY = y # never change y axis
+    else:
+        if rapidMode:
+            # print('* move: (X:'+str(x)+', Y:'+str(y)+')')
+            moveRelativeToAbsolute(x,y)
+        else:
+            # print('* draw: (X:'+str(x)+', Y:'+str(y)+')')
+            drawLineFromRelativeToAbsolute(x,y)
 
-    pyglet.image.get_buffer_manager().get_color_buffer().save(filename+'.png')
+    continue
 
-pyglet.app.run()
+for polygonTuples in polygons:
+    if len(polygonTuples) < 1:
+        continue
+    print(polygonTuples)
+    draw.polygon(polygonTuples, fill=(255,255,255,255))
+
+# im.show()
+im.save(filename+'.pil.png')
