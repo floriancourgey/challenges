687a4a4a659e5ee953ff0a76eadb6e757ef547cd
Florian Courgey <floriancourgey@users.noreply.github.com>
2018-08-15 14:11:25 -0400

    * prog/gravure

--- programmation/La gravure mécanique/main.py
+++ programmation/La gravure mécanique/main.py
@@ -8,14 +8,15 @@ import time
 window = pyglet.window.Window(1200, 300)
 
 i=0 # number of lines drawn
-facteur = 20 # scale text smaller/bigger
+rapidMode = True # True = don't draw, just move / False = draw
+scaleFactor = 20 # scale text smaller/bigger
 f = open('samples/1.gcode', 'r')
 
 relativeX = 0 # last x (modified by moveRelativeToAbsolute() )
 relativeY = 0 # last y (modified by moveRelativeToAbsolute() )
 
 absoluteX = 10 # current x origin (modified by G52)
-absoluteY = 30 # current y origin (modified by G52)
+absoluteY = scaleFactor*4 # current y origin (modified by G52)
 
 def drawLineAbsolute(fromX, fromY, toX, toY):
     global absoluteX
@@ -24,19 +25,20 @@ def drawLineAbsolute(fromX, fromY, toX, toY):
     i+=1
     pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
         ('v2f', (fromX+absoluteX, fromY+absoluteY, toX+absoluteX, toY+absoluteY)),
-        ('c3B', (0, 255, 0, 255, 0, 255))
+        ('c3B', (255, 255, 255, 255, 255, 255)) # white
+        # ('c3B', (0, 255, 0, 255, 0, 255)) # green to purple
     )
     print('draw abs', (fromX+absoluteX, fromY+absoluteY, toX+absoluteX, toY+absoluteY))
-    # on trace 1 i sur 2
-    if i%2 == 1:
-        pyglet.text.Label(text=str(i), x=fromX+absoluteX, y=fromY+absoluteY).draw()
+    # draw a text helper at the start of the line (for debugging)
+    # if i%2 == 1:
+    #     pyglet.text.Label(text=str(i), x=fromX+absoluteX, y=fromY+absoluteY).draw()
 
 def drawLineFromRelativeToAbsolute(toX, toY):
     global relativeX
     global relativeY
-    if toX == 0:
+    if toX is None:
         toX = relativeX
-    if toY == 0:
+    if toY is None:
         toY = relativeY
     drawLineAbsolute(relativeX, relativeY, toX, toY)
     moveRelativeToAbsolute(toX, toY)
@@ -44,10 +46,11 @@ def drawLineFromRelativeToAbsolute(toX, toY):
 def moveRelativeToAbsolute(toX, toY):
     global relativeX
     global relativeY
-    if toX != 0:
+    if toX is not None:
         relativeX = toX
-    if toY != 0:
+    if toY is not None:
         relativeY = toY
+
 
 @window.event
 def on_draw():
@@ -55,23 +58,42 @@ def on_draw():
     global absoluteY
 
     window.clear()
-    regex = "(?P<g52>G52)?(X(?P<x>[\-\d\.]+))?(Y(?P<y>[\-\d\.]+))?"
-    for match in re.finditer(regex, f.read()):
-        x = y = 0
+    regex = "(?P<g0>G0)?(?P<g1>G1)?(?P<g52>G52)?(X(?P<x>[\-\d\.]+))?(Y(?P<y>[\-\d\.]+))?"
+    for iLine,line in enumerate(f):
+        line = line.strip().upper() # strip + upper
+        print('- line '+str(iLine+1)+':', line)
+        match = re.match(regex, line)
+        # Rapid mode
+        if not match:
+            continue
+        if match.group('g0'):
+            print('* G0: Rapid mode ON')
+            rapidMode = True
+            continue
+        if match.group('g1'):
+            print('* G1: Rapid mode OFF')
+            rapidMode = False
+            continue
+        # X/Y with or without G52
+        x = y = None
         if match.group('x'):
-            x = round(float(match.group('x'))*facteur, 1)
+            x = round(float(match.group('x'))*scaleFactor, 1)
         if match.group('y'):
-            y = round(float(match.group('y'))*facteur, 1)
-        if x==0 and y==0:
+            y = round(float(match.group('y'))*scaleFactor, 1)
+        if x==None and y==None:
             continue
         # change origin
         if match.group('g52'):
-            print('- line "'+match.group(0).strip()+'": G52 :', x, y)
+            print('* G52: (X:'+str(x)+', Y:'+str(y)+')')
             absoluteX = x
-            # absoluteY = y # we don't want to change the y axis
+            # absoluteY = y # never change y axis
         else:
-            print('- line "'+match.group(0).strip()+'": draw :', x, y)
-            drawLineFromRelativeToAbsolute(x,y)
+            if rapidMode:
+                print('* move: (X:'+str(x)+', Y:'+str(y)+')')
+                moveRelativeToAbsolute(x,y)
+            else:
+                print('* draw: (X:'+str(x)+', Y:'+str(y)+')')
+                drawLineFromRelativeToAbsolute(x,y)
 
         continue
 
