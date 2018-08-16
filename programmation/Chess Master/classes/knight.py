2036681919074deb33beb466b718cefce01f20af
Florian courgey (Fresh) <florian@floriancourgey.com>
2018-08-16 16:47:04 -0400

    + permutations Knight

--- programmation/Chess Master/classes/knight.py
+++ programmation/Chess Master/classes/knight.py
@@ -2,9 +2,17 @@
 # coding: utf-8
 from .piece import Piece
 from .square import Square
+from itertools import permutations
 
 class Knight(Piece):
     def type(self):
         return "c"
-    def squaresAvailableFromTarget(self):
-        return []
+    def getPermutations(self):
+        couples = []
+        for x,y in permutations([-2,-1,1,2], 2): # [(-2,-2), (-2,-2), (-2,1)...]
+            # remove (2,2) and (1,1), so we have:
+            # [removed, (-2,-2), (-2,1)...]
+            if abs(x)+abs(y) != 3:
+                continue
+            couples.append((x,y))
+        return couples
