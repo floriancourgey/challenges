b435e1365e6f68c2e2f6c893223ee9756b78079e
Florian Courgey <florian@floriancourgey.com>
2018-08-19 22:16:38 -0400

    * capturesAvailableToTarget now takes target's square instead of target itself

--- programmation/Chess Master/levels/level_2.py
+++ programmation/Chess Master/levels/level_2.py
@@ -4,13 +4,16 @@ from classes.pawn import Pawn
 
 class Level_2:
     def __init__(self, target, attacker):
-        print('Level 2 init for target=',target,' and attacker=', attacker)
+        print('Level 2 init for target=',target,'and attacker=', attacker)
         self.target, self.attacker = target, attacker
     def solve(self):
         pawn = Pawn()
-        squarePawn = pawn.capturesAvailableToTarget(self.target, [], True)
+        squarePawn = pawn.capturesAvailableToTarget(self.target.square, [], True)
         pawn.moveToSquare(squarePawn)
-        squareAttacker = self.attacker.capturesAvailableToTarget(pawn, self.target.movesAvailable(), True)
+        blacklist = self.target.movesAvailable()
+        blacklist.append(squarePawn)
+        blacklist.append(self.target.square)
+        squareAttacker = self.attacker.capturesAvailableToTarget(pawn.square, blacklist, True)
         self.attacker.moveToSquare(squareAttacker)
         solution = pawn, self.attacker
         print('Level 2 solved with solution:', solution)
