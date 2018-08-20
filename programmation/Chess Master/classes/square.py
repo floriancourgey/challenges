29dd9a8334c52387243e041a4ca56a33573143a3
Florian Courgey <florian@floriancourgey.com>
2018-08-19 22:16:02 -0400

    + square helpers (toTuple, add, sub, possibility to be < 0 or > 7)

--- programmation/Chess Master/classes/square.py
+++ programmation/Chess Master/classes/square.py
@@ -3,12 +3,18 @@
 
 class Square:
     def __init__(self, x=0, y=0):
-        if(x<0 or x>7 or y<0 or y>7):
-            raise ValueError("Wrong params ("+str(x)+","+str(y)+"): 0 <= x,y <= 7")
+        # if(x<0 or x>7 or y<0 or y>7):
+            # raise ValueError("Wrong params ("+str(x)+","+str(y)+"): 0 <= x,y <= 7")
         self.x, self.y = x, y
         self.letters = 'ABCDEFGH'
+    def toTuple(self):
+        return self.x, self.y
     def __str__(self):
         return self.letters[self.x]+str(self.y+1)
     ''' 2 squares are equal is they have the same (x,y) '''
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
+    def __add__(self, other):
+        return Square(self.x+other.x, self.y+other.y)
+    def __sub__(self, other):
+        return Square(self.x-other.x, self.y-other.y)
