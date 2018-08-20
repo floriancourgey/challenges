4118d6ddc5e3095ad3ab27a6eba7e8b818a208bc
Florian Courgey <florian@floriancourgey.com>
2018-08-19 22:21:12 -0400

    + prog/chess level 3 20%

--- programmation/Chess Master/main.py
+++ programmation/Chess Master/main.py
@@ -2,7 +2,9 @@
 # coding: utf-8
 import config
 import re
+import time
 import requests
+from classes.square import Square
 from classes.piece import Piece
 from classes.bishop import Bishop
 from classes.knight import Knight
@@ -12,6 +14,7 @@ from classes.queen import Queen
 from classes.king import King
 from levels.level_1 import Level_1
 from levels.level_2 import Level_2
+from levels.level_3 import Level_3
 
 # coordinates start in bottom-left corner from (0,0) to (7,7)
 # we are white (bottom A-H 0-1)
@@ -24,34 +27,58 @@ def callNC(url):
     return r.text
 
 def createFromString(s):
-    s = s.lower()
+    s = s.lower().replace(' ', '')
     if s == "pion": return Pawn()
     if s == "cavalier": return Knight()
     if s == "fou": return Bishop()
     if s == "tour": return Rook()
     if s == "dame": return Queen()
 
-target = King()
-target.getPermutationsMove()
 l1url = 'https://www.newbiecontest.org/epreuves/prog/prog_chessmaster1.php'
-l1regex = re.compile('; en (?P<target>[A-H][1-8])\..+<br\/>(?P<attacker>\w+)<br')
+l1regex = re.compile('; en ([A-H][1-8])\..+<br\/>(\w+)<br')
+l2regex = re.compile('en ([A-H][1-8]) tout en.+>(\w+)<br\/><\/p>')
+l3regex = re.compile('en ([A-H][1-8]) .+ : <br/>(.+)<br/><\/p>')
+
+target = King()
+
 l1data = callNC(l1url)
 m = l1regex.search(l1data)
-print('Level 1 data: Target ', m.group('target'), ', attacker', m.group('attacker'))
-target.moveToNotation(m.group('target'))
-attacker = createFromString(m.group('attacker'))
+if not m:
+    exit('Level 1 error')
+print('Level 1 data: Target', m.group(1), ', attacker', m.group(2))
+target.moveToNotation(m.group(1))
+attacker = createFromString(m.group(2))
 l1 = Level_1(target, attacker)
 solution = l1.solve()
 l2data = callNC(l1url+'?struct='+str(solution))
-print(l2data)
-
-l2regex = re.compile('en (?P<target>[A-H][1-8]) tout en.+>(?P<attacker>\w+)<br\/><\/p>')
+# print(l2data)
+# LEVEL 2
 m = l2regex.search(l2data)
-print('Level 2 data: Target ', m.group('target'), ', attacker', m.group('attacker'))
-
-target.moveToNotation(m.group('target'))
-attacker = createFromString(m.group('attacker'))
+if not m:
+    exit('Level 2 error')
+print('Level 2 data: Target', m.group(1), ', attacker', m.group(2))
+target.moveToNotation(m.group(1))
+attacker = createFromString(m.group(2))
 l2 = Level_2(target, attacker)
 solution = l2.solve()
 l3data = callNC(l1url+'?struct='+str(solution[0])+str(solution[1]))
 print(l3data)
+# LEVEL 3
+m = l3regex.search(l3data)
+if not m:
+    exit('Level 3 error')
+attackers = []
+sAttackers = m.group(2).split('<br/>')
+for sAttacker in sAttackers:
+    attackers.append(createFromString(sAttacker))
+attackers = sorted(attackers, key=lambda x: x.value, reverse=True)
+print('Level 3 data: Target', m.group(1), '- attackers:',attackers)
+with open("results.txt", "a") as myfile:
+    myfile.write('Level 3 data: Target\t'+m.group(1)+'\tattackers\t'+' '.join(list(map(lambda x:x.typeFull, attackers)))+'\n')
+
+# target.moveToNotation(m.group(1))
+# attackers = [createFromString(m.group(2)), createFromString(m.group(3)), createFromString(m.group(4))]
+# l3 = Level_3(target, attackers)
+# solution = l3.solve()
+# l4data = callNC(l1url+'?struct='+str(solution[0])+str(solution[1])+str(solution[2]))
+# print(l4data)
