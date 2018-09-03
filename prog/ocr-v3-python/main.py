a06ff06adc4521b18077c653346919ebad085060
Florian Courgey <florian@floriancourgey.com>
2018-09-03 23:20:40 +0200

    + prog/ocr-v3 rename to main.py

--- prog/ocr-v3-python/main.py
+++ prog/ocr-v3-python/main.py
@@ -1,19 +1,26 @@
 #! /usr/bin/env python3
 # coding: utf-8
-import requests
+import sys
+sys.path.insert(0, '../..')
+from config import *
+from functions import *
 from PIL import Image, ImageFilter
 import config
 import cv2
+from datetime import datetime
 
 pngSignature = b'\x89\x50\x4E\x47'
 
+filename = 'samples/2018-09-03T18:06:36.096125file.png'
 # offline
-f = open('original/imgcrypted.png', 'rb')
+f = open(filename, 'rb')
 bin_data = f.read()
 # online
-# r = requests.get(URL:config.COOKIE})
-# with open('results/file.png', "wb") as local_file:
-#     local_file.write(r.content)
+# bin_data = get(config.URLS['prog']['ocr-v3']['problem'], True).content
+# filename = 'results/'+datetime.now().isoformat()+'file.png'
+# with open(filename, "wb") as local_file:
+#     local_file.write(bin_data)
+#     print(filename+' ok')
 # split whole file by PNG signature
 chunks = bin_data.split(pngSignature) # ['', file1, file2, file3]
 if len(chunks) != 4:
@@ -21,18 +28,18 @@ if len(chunks) != 4:
 del chunks[0] # remove first empty
 # create the 3 files
 for i,chunk in enumerate(chunks):
-    with open("results/file"+str(i)+".png", "wb") as file:
+    with open(filename+str(i)+".png", "wb") as file:
         file.write(pngSignature)
         file.write(chunk)
-master = Image.open("results/file0.png")
+master = Image.open(filename+"0.png")
 print('size(x,y)', master.size)
 xMax = master.size[0]
 yMax = master.size[1]
-vertical = Image.open("results/file1.png")
-horizontal = Image.open("results/file2.png")
+vertical = Image.open(filename+"1.png")
+horizontal = Image.open(filename+"2.png")
 # create output file, with the size of the master
 output = Image.new('RGB', master.size)
-output.save("results/output.png") # create black PNG file
+output.save(filename+"final.png") # create black PNG file
 pixels = output.load() # create the pixel map
 # for each pixel(x,y) in master
 for y in range(yMax):
@@ -53,19 +60,19 @@ for y in range(yMax):
             pixels[xNew,yNew] = (255, 255, 255)
     # break
 
-output.save("results/output.png") # save pixels modification
+output.save(filename+"final.png") # save pixels modification
 
-output2 = output.resize((xMax*5, yMax*5))
-output2.save("results/output_5.png")
+# output2 = output.resize((xMax*5, yMax*5))
+# output2.save("results/output_5.png")
 
-image = cv2.imread("results/output_5.png", 0)
-image = cv2.medianBlur(image, 5)
+# image = cv2.imread("results/output_5.png", 0)
+# image = cv2.medianBlur(image, 5)
 # image = cv2.GaussianBlur(image,(5,5),0)
 # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-image = cv2.threshold(image, 10, 255, cv2.THRESH_BINARY)[1]
+# image = cv2.threshold(image, 10, 255, cv2.THRESH_BINARY)[1]
 # a,b = cv2.threshold(image,127,255,cv2.THRESH_TOZERO)
 # image = cv2.bilateralFilter(image,9,75,75)
-cv2.imwrite("results/output_blur.png", image)
+# cv2.imwrite("results/output_blur.png", image)
 # image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 # cv2.imwrite("results/output_tresh.png", image)
 # print(r,g,b)
