8d4c27ef73ba481e6ec4e599fd5f33b3c9bc5894
Florian Courgey <florian@floriancourgey.com>
2018-09-04 19:38:25 -0400

    + prog/ocr-v3 final step with vigenere (3/3)

--- prog/ocr-v3-python/main.py
+++ prog/ocr-v3-python/main.py
@@ -19,16 +19,16 @@ pngSignature = b'\x89\x50\x4E\x47'
 ocr = OcrMonotype(8, 10, 5, 8, 0, 5, 'dico_8_10.txt', True)
 
 # offline
-filename = 'samples/2018-09-03T18:06:36.096125file.png'
-print('Offline mode with file: '+filename)
-f = open(filename, 'rb')
-bin_data = f.read()
+# filename = 'samples/2018-09-03T18:06:36.096125file.png'
+# print('Offline mode with file: '+filename)
+# f = open(filename, 'rb')
+# bin_data = f.read()
 # online
-# bin_data = get(config.URLS['prog']['ocr-v3']['problem'], True).content
-# filename = 'results/'+datetime.now().isoformat()+'file.png'
-# with open(filename, "wb") as local_file:
-#     local_file.write(bin_data)
-#     print(filename+' ok')
+bin_data = get(config.URLS['prog']['ocr-v3']['problem'], True).content
+filename = 'results/'+datetime.now().isoformat()+'file.png'
+with open(filename, "wb") as local_file:
+    local_file.write(bin_data)
+    print(filename+' ok')
 # split whole file by PNG signature
 chunks = bin_data.split(pngSignature) # ['', file1, file2, file3]
 if len(chunks) != 4:
@@ -57,7 +57,6 @@ for y in range(yMax):
         if r+g+b == 0:
             continue
         if r>0:
-            # print('pixel found (x='+str(x)+',y='+str(y)+')', r, g, b)
             # horizontal offset first
             deltaY = horizontal.getpixel((x,0))[0]
             yNew = (y-deltaY)%yMax
@@ -65,7 +64,6 @@ for y in range(yMax):
             deltaX = vertical.getpixel((0,yNew))[0]
             xNew = (x-deltaX)%xMax
             # write it
-            # pixels[xNew,yNew] = (r, 0, 0)
             pixels[xNew,yNew] = (255, 255, 255)
 
 output.save(finalFilename) # save pixels modification
@@ -76,32 +74,17 @@ lines = ocr.compute()
 print('OCR data: ', lines)
 if len(lines) != 3:
     raise Exception('OCR Unable to find 3 lines in OCR data')
-words = lines[0].split(' ')
-key = lines[1]
+sentence = lines[0].strip()
+key = lines[1].strip()
 index = int(lines[2][0])
-word = words[index-1]
-if len(lines) != 3:
-    raise Exception('OCR Unable to find word with index0 '+str(index-1)+' in line 1')
-print('Vigenere decypher for word: '+word, 'with key: '+key)
+index0 = index-1
+print('Vigenere decypher for sentence: '+sentence, 'with key: '+key)
 vigenere = Vigenere(key)
-result = vigenere.decrypt(word)
+result = vigenere.decrypt(sentence)
 print('Vigenere result: '+result)
-
-html = get(config.URLS['prog']['ocr-v3']['solution']+'?solution='+result)
+words = result.split(' ')
+if index0 >= len(words):
+    exit('Index mismatch with number of words')
+solution = words[index0]
+html = get(config.URLS['prog']['ocr-v3']['solution']+'?solution='+solution)
 print(html)
-
-# output2 = output.resize((xMax*5, yMax*5))
-# output2.save("results/output_5.png")
-
-# image = cv2.imread("results/output_5.png", 0)
-# image = cv2.medianBlur(image, 5)
-# image = cv2.GaussianBlur(image,(5,5),0)
-# image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# image = cv2.threshold(image, 10, 255, cv2.THRESH_BINARY)[1]
-# a,b = cv2.threshold(image,127,255,cv2.THRESH_TOZERO)
-# image = cv2.bilateralFilter(image,9,75,75)
-# cv2.imwrite("results/output_blur.png", image)
-# image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-# cv2.imwrite("results/output_tresh.png", image)
-# print(r,g,b)
-# print(master.mode)
