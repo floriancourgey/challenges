79c8458f2e40b33892dfd84718a3f5b1a62ccf46
Florian Courgey <florian@floriancourgey.com>
2018-09-04 21:36:40 -0400

    ! db params & config.php

--- hack/forum1/include/config.php
+++ hack/forum1/include/config.php
@@ -1,16 +1,11 @@
 <?php
-// a changer
-$dbhost = "localhost";
-$dbname="dbname";
-$dbuser = "user";
-$dbpass = "pass";
-// fin des changements a faire
+require_once 'db_params.php';
 $allowed_pages_log=array("memberlist","logout","profile");
 $allowed_pages_nolog=array("register","login");
 if (file_exists("include/fonctions.php")) include("include/fonctions.php");
 else die("Il manque un fichier");
-$db = mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
-mysql_select_db($dbname, $db) or die(mysql_error());
+$db = @mysql_connect(HOST, USER, PASS) or die(mysql_error());
+mysql_select_db(DB, $db) or die(mysql_error());
 appel("include/header.php");
 appel("langue/" . LANGUAGE . ".php");
 ?>
