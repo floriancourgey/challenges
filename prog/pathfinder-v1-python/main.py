2e39b7629615dab9c9733e07e2e738d88b134b45
Florian Courgey <florian@floriancourgey.com>
2018-09-05 21:23:02 -0400

    + pathfinder networkx

--- prog/pathfinder-v1-python/main.py
+++ prog/pathfinder-v1-python/main.py
@@ -8,6 +8,9 @@ from PIL import Image, ImageFilter
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
+import numpy
+import networkx as nx
+import matplotlib.pyplot as plt
 
 X_TILE = 5 # a tile is (5,5) wide
 Y_TILE = 5
@@ -17,13 +20,16 @@ TILE_START = (0, 0, 255)
 TILE_END = (255, 0, 0)
 
 im = Image.open("original/maze.png")
+# im = Image.open("samples/1.png")
 
 print('image(x,y)', im.size)
 xMax = int(im.size[0] / X_TILE) # define horizontal number of tiles
 yMax = int(im.size[1] / Y_TILE) # define vertical number of tiles
+max = max(xMax, yMax)
 print('tiles(x,y)', (xMax,yMax))
 
-matrix = [[0 for x in range(xMax)] for y in range(yMax)] # create matrix of 0
+# matrix = [[0 for x in range(xMax)] for y in range(yMax)] # create matrix of 0
+matrix = [[0 for x in range(max)] for y in range(max)] # create matrix of 0
 pixels = im.load() # get the pixel map
 # for each tile(x,y)
 for y in range(yMax):
@@ -40,13 +46,25 @@ for y in range(yMax):
             END = (x,y)
 
 # start A*
-grid = Grid(matrix=matrix)
+# grid = Grid(matrix=matrix)
+#
+# start = grid.node(START[0], START[1])
+# end = grid.node(END[0], END[1])
+#
+# finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+# path, runs = finder.find_path(start, end, grid)
+#
+# print('operations:', runs, 'path length:', len(path))
+# print(grid.grid_str(path=path, start=start, end=end))
 
-start = grid.node(START[0], START[1])
-end = grid.node(END[0], END[1])
+# start networkx
+A=numpy.matrix(matrix)
+G=nx.from_numpy_matrix(A)
+G.edges(data=True)
+print(G.number_of_nodes())
+print(G.graph)
 
-finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
-path, runs = finder.find_path(start, end, grid)
-
-print('operations:', runs, 'path length:', len(path))
-print(grid.grid_str(path=path, start=start, end=end))
+G = nx.petersen_graph()
+# plt.subplot(121)
+nx.draw(G, with_labels=True, font_weight='bold')
+plt.show()
