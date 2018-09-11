0463532c9c232e471123a58664fb2ae8abdc62e0
Florian courgey (Fresh) <florian@floriancourgey.com>
2018-09-11 11:19:32 -0400

    + prog/bars 95%

--- prog/bars/main.py
+++ prog/bars/main.py
@@ -3,12 +3,18 @@
 import sys
 sys.path.insert(0, '../..')
 from pyquery import PyQuery as pq
-# from lxml import etree
+from functions import *
 from bar import Bar
+from datetime import datetime
 
-# f = open('sample.txt', 'r')
-# lines = f.readlines()
-d = pq(filename='sample1.html')
+# d = pq(filename='sample1.html')
+filename = 'samples/'+datetime.now().isoformat().replace(':', '')+'.html'
+local_file = open(filename, "w", encoding="utf-8")
+html = get(config.URLS['prog']['bars']['problem'], True).content.decode('utf-8')
+d = pq( html )
+# print(html)
+# save to file
+local_file.write(html)
 
 bars = {}
 drinks = [] # drinks taken
@@ -29,16 +35,16 @@ if len(bars)<10:
     raise ValueError('Incorrect number of bars')
 # print(bars)
 # get first bar
-FIRST_BAR = int(body.find('span.bar_initial').text())
-if FIRST_BAR < 1:
+FIRST_BAR_ID = int(body.find('span.bar_initial').text())
+if FIRST_BAR_ID < 1:
     raise ValueError('Incorrect value for initial bar')
-print('Initial bar:', FIRST_BAR)
+print('Initial bar:', FIRST_BAR_ID)
 # get drinks list
 for li in body.find('ul.consos>li'):
     drinks.append(d(li).text())
 if len(drinks)<10:
     raise ValueError('Incorrect number of drinks')
-print('Drinks taken:', drinks)
+print('Drinks taken:', len(drinks), drinks)
 # get drinks price for each bar
 menus = body.find('table.menu')
 for i,menu in enumerate(menus):
@@ -52,16 +58,16 @@ for id,bar in bars.items():
     if len(bar.menu) != NB_DRINKS:
         raise ValueError('Invalid number of drinks for bar', bar)
 
-def getProbas(bars, initialBar, nextDrink):
+def getProbas(bars, previousBar, nextDrink):
     probas = {}
     minDist = 99
     minPrice = 99
     for id,bar in bars.items():
-        if bar == initialBar:
+        if bar == previousBar:
             continue
         probas[bar.id] = {
             'bar': bar,
-            'distanceAbs':float(initialBar.distanceToBar(bar)), # absolute distance
+            'distanceAbs':float(previousBar.distanceToBar(bar)), # absolute distance
             'priceAbs':float(bar.menu[nextDrink]), # absolute price
         }
         if probas[bar.id]['distanceAbs'] < minDist:
@@ -81,11 +87,19 @@ def getProbas(bars, initialBar, nextDrink):
         if proba['Px'] > maxPx:
             maxProba = proba
             maxPx = proba['Px']
-    print(probas)
+    # print(probas)
     return maxProba
 
-initialBar = bars[FIRST_BAR]
-for drink in drinks[0:1]:
-    print('- Get Proba for drink:', drink, 'and bar:', initialBar)
-    print(getProbas(bars, initialBar, drink))
-    # initialBar = min(probas)
+solution = [str(FIRST_BAR_ID)]
+previousBar = bars[FIRST_BAR_ID]
+for drink in drinks:
+    print('- Get Proba for drink:', drink, 'and bar:', previousBar)
+    maxProba = getProbas(bars, previousBar, drink)
+    print('maxProba:', maxProba)
+    previousBar = maxProba['bar']
+    solution.append(str(previousBar.id))
+print(len(solution))
+solution = '-'.join(solution[:-1])
+print(solution)
+html = get(config.URLS['prog']['bars']['solution']+'?sequence='+solution)
+print(html)
