7a0240f3217d87ddbe1e6606135685b8ca5e364a
Florian courgey (Fresh) <florian@floriancourgey.com>
2019-01-30 17:38:51 -0500

    + prog/mib 85%

--- prog/mib/main.py
+++ prog/mib/main.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 import subprocess
 import os
+import re
 MIB_FOLDER = os.getcwd() # should contain NC-MIB.txt and SNMPv2-SMI
-ret = subprocess.run(['snmpwalk', '-M', MIB_FOLDER, '-t', '10', '-Os', '-c', 'public', '-v', '2c', 'www.***.org', '1.3.6.1.4.1.9999.2.1.3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode('utf-8')
+MIB_WALK = ['snmpwalk', '-M', MIB_FOLDER, '-t', '10', '-Os', '-c', 'public', '-v', '2c', 'www.***.org', '1.3.6.1.4.1.9999.2.1.3']
+ret = subprocess.run(MIB_WALK, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode('utf-8')
 for line in ret.split('\n'):
-	if 'INTEGER: 99' in line:
-		print(line)
+	#if 'INTEGER: 99' in line:
+	m = re.search('^.+\.(\w+) = INTEGER: 99', line)
+	if not m:
+		continue
+	print(line)
+	mib_id = m.group(1)
+	print('mib_id', mib_id)
+	MIB_WALK[-1] = "1.3.6.1.4.1.9999.2.1.1."+mib_id
+	print('MIB_WALK', MIB_WALK)
+	ret = subprocess.run(MIB_WALK, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode('utf-8')
+	print('ret', ret)
+	m = re.search('^.+ = INTEGER: (\w+)', ret)
+	cpu = m.group(1)
+	print('cpu', cpu)
